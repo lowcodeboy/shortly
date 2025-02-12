aa3ee5320882c302b2b9661dde14d896e8014f17
Florent Burdese <florentburdese@Host-003.lan>
2025-02-12 17:24:45 +0100

    firstnew

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { geist, geistMono } from './fonts'
 import "./globals.css";
 import { AuthProvider } from './context/AuthContext';
 import AuthRedirect from './components/auth/AuthRedirect';
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,10 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={`${geist.variable} ${geistMono.variable} antialiased`}>
+      <body>
         <AuthProvider>
           <AuthRedirect>
             {children}
